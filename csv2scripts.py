052990371ee66cac2242f88f011ce99385b02919
David Boddie <david@boddie.org.uk>
2022-06-17 22:43:34 +0200

    Added scripts and an initial test case.

---
+++ csv2scripts.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""
+Copyright (C) 2022 David Boddie <david@boddie.org.uk>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os, sys
+
+check_headings = ["Status", "Name", "Publisher", "UEF", "ROMs", "Options", "URL", "Files"]
+
+lines = open("roms.csv").readlines()
+
+headings = lines.pop(0).strip().split(",")
+if headings != check_headings:
+    sys.stderr.write("Headings in the roms.csv file were not as expected.\n")
+    sys.exit(1)
+
+bf = open("batch.sh", "w")
+bf.write("#!/bin/sh\n\nset -e\n\n")
+tf = open("testroms.sh", "w")
+tf.write("#!/bin/sh\n\nset -e\n\n")
+
+games = 0
+roms = 0
+
+for line in lines:
+
+    pieces = line.strip().split(",")
+    if len(pieces) != len(check_headings):
+        print("Invalid entry:", pieces)
+        continue
+    
+    d = {}
+    for key, value in zip(check_headings, pieces):
+        d[key] = value
+    
+    if not d["Status"].startswith("OK"):
+        print("Skipping", d["Name"], "-", d["Status"])
+        continue
+    
+    elif d["URL"] == "-":
+        print("Skipping", d["Name"])
+        continue
+    
+    games += 1
+    
+    ROMs = d["ROMs"].split()
+    for i, ROM in enumerate(ROMs):
+        d["ROM%i" % (i + 1)] = ROM
+    
+    if len(ROMs) == 1:
+        bf.write("UEF2ROM.py %(Options)s UEFs/%(UEF)s ROMs/%(ROM1)s\n" % d)
+        tf.write("elkulator -rom2 ROMs/%(ROM1)s\n" % d)
+    elif len(ROMs) == 2:
+        bf.write("UEF2ROM.py %(Options)s UEFs/%(UEF)s ROMs/%(ROM1)s ROMs/%(ROM2)s\n" % d)
+        tf.write("elkulator -rom2 ROMs/%(ROM1)s -rom1 ROMs/%(ROM2)s\n" % d)
+    else:
+        d["ROMs"] = " ".join(map(lambda ROM: "ROMs/" + ROM, ROMs))
+        d["combined"] = "ROMs/" + os.path.split(d["UEF"])[1].replace(".uef", ".rom")
+        bf.write("UEF2ROM.py %(Options)s UEFs/%(UEF)s %(ROMs)s\n" % d)
+        bf.write("cat %(ROMs)s > %(combined)s\n" % d)
+        tf.write("elkulator -rom2 %(combined)s\n" % d)
+    
+    roms += len(ROMs)
+
+bf.close()
+tf.close()
+
+print("%i titles" % games)
+print("%i ROMs" % roms)
---
+++ fetch-uefs.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+"""
+Copyright (C) 2019 David Boddie <david@boddie.org.uk>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os, requests, sys, time, zipfile
+from io import BytesIO
+
+check_headings = ["Status", "Name", "Publisher", "UEF", "ROMs", "Options", "URL", "Files"]
+
+lines = open("roms.csv").readlines()
+
+headings = lines.pop(0).strip().split(",")
+if headings != check_headings:
+    sys.stderr.write("Headings in the roms.csv file were not as expected.\n")
+    sys.exit(1)
+
+uef_dir = "UEFs"
+
+if not os.path.isdir(uef_dir):
+    os.mkdir(uef_dir)
+
+new_lines = []
+new_lines.append(",".join(check_headings))
+
+for line in lines:
+
+    pieces = line.strip().split(",")
+    if len(pieces) != len(check_headings):
+        print("Invalid entry:", pieces)
+        continue
+    
+    d = {}
+    for key, value in zip(check_headings, pieces):
+        d[key] = value
+    
+    if not d["Status"].startswith("OK"):
+        print("Skipping", d["Name"], "-", d["Status"])
+        continue
+    
+    file_names = d["Files"].split()
+    original_url = url = d["URL"].strip()
+    
+    found = True
+    for file_name in file_names:
+        file_name = os.path.split(file_name)[1]
+        if not os.path.exists(os.path.join(uef_dir, file_name)):
+            found = False
+            break
+    
+    if not found and url != "-":
+    
+        print("Downloading", url)
+        
+        resp = requests.get(url)
+        resp.close()
+
+        if not resp.ok:
+            sys.stderr.write("Failed to download %s\n" % file_name)
+            
+            # Don't try to download this UEF next time.
+            if not original_url:
+                d["URL"] = "-"
+        
+        data = BytesIO(resp.content)
+        d["URL"] = url
+        
+        try:
+            zf = zipfile.ZipFile(data)
+            for file_name in file_names:
+                data = zf.read(file_name)
+                open(os.path.join(uef_dir, os.path.split(file_name)[1]),
+                     "wb").write(data)
+        
+        except KeyError:
+            sys.stderr.write("Failed to find %s in the archive.\n" % file_name)
+            sys.stderr.write("Found: %s\n" % " ".join(zf.namelist()))
+            
+            # Don't try to download this UEF next time.
+            if not original_url:
+                d["URL"] = "-"
+        
+        time.sleep(1)
+    
+    new_line = []
+    for heading in check_headings:
+        new_line.append(d[heading])
+    
+    new_lines.append(",".join(new_line))
+
+open("roms.csv", "w").write("\n".join(new_lines) + "\n")
